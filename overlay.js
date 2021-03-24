9277f2b81da0f14aa7a6cf723eed5b50dc6ec91e
Yoni Touboul <yonitouboul@hotmail.com>
2021-03-24 19:35:25 +0100

    Fix left scroll translation

--- overlay.js
+++ overlay.js
@@ -5,7 +5,7 @@ const accordions = document.querySelectorAll('.accordion-item h4');
 
 hamburger.forEach(button => {
   button.addEventListener('click', e => {
-    document.querySelector('#menu').classList.toggle('open');
+    fadeOverlay(document.querySelector('#menu'), document.querySelector('#ticket'));
     hamburger[0].classList.toggle('active');
     hamburger[1].classList.toggle('active');
   });
@@ -13,20 +13,16 @@ hamburger.forEach(button => {
 
 accordions.forEach(accordion => {
   accordion.addEventListener('click', e => {
-    let activeAccordion = Array.from(document.querySelectorAll('.accordion-item h4')).find(accordion => accordion.parentNode.classList.contains('active'))
-    if (activeAccordion) {
-      activeAccordion.parentNode.classList.remove('active');
-    }
-    accordion.parentNode.classList.add('active');
+    let activeAccordion = Array.from(document.querySelectorAll('.accordion-item h4')).find(accordion => accordion.parentNode.classList.contains('active'));
+    toggleAccordion(activeAccordion, accordion)
   });
 });
 
 titles.forEach((title, index) => {
   title.addEventListener('click', e => {
     initiateView(title, document.querySelector('#back-button'));
-    translateTitles(index, titles, title)
     translateContent(title.parentNode.querySelector('section'));
-
+    translateTitles(index, Array.from(titles).filter(t => t !== title), title)
   });
 });
 
@@ -38,56 +34,51 @@ backButton.addEventListener('click', e => {
   });
 });
 
+const fadeOverlay = (menu, panel) => {
+  if (menu.classList.contains('open')) {
+    panel.classList.toggle('no-height')
+  } else {
+    setTimeout(e => panel.classList.toggle('no-height'), 500);
+  }
+  menu.classList.toggle('open');
+}
+
+const toggleAccordion = (active, element) => {
+  if (active) {
+    active.parentNode.classList.remove('active');
+  }
+  element.parentNode.classList.add('active');
+}
+
 const initiateView = (title, button) => {
   title.style.pointerEvents = "none";
   button.style.opacity = 1;
 };
+
 const translateTitles = (position, titles, clicked_title) => {
-  titles.forEach(title => {
-    if (title !== clicked_title) {
-      title.classList.add('no-height');
-      title.parentNode.classList.add('no-height');
-    }
-  });
   if (window.innerWidth <= 780) {
     document.querySelector('.languages').classList.add('hidden');
     document.querySelector('.menu-left').classList.add('visible');
   }
-  if (position === 0) {
-    titles[1].style.transform = "translateY(80vh)"
-    titles[2].style.transform = "translateY(90vh)"
-    titles[3].style.transform = "translateY(100vh)"
-  } else if (position === 1) {
-    titles[0].style.transform = "translateY(-70vh)"
-    titles[2].style.transform = "translateY(80vh)"
-    titles[3].style.transform = "translateY(90vh)"
-  } else if (position === 2) {
-    titles[0].style.transform = "translateY(-70vh)"
-    titles[1].style.transform = "translateY(-80vh)"
-    titles[3].style.transform = "translateY(80vh)"
-  } else {
-    titles[0].style.transform = "translateY(-80vh)"
-    titles[1].style.transform = "translateY(-90vh)"
-    titles[2].style.transform = "translateY(-100vh)"
-  }
+   titles.forEach((title, index) => {
+    title.style.transform = `translateY(${index < position ? '-' : ''}100vh)`;
+    title.parentNode.classList.add('no-height');
+  });
 };
 
 const translateContent = (content, title) => {
   content.style.opacity = 1;
-  content.style.height = '70vh';
+  content.style.height = '60vh';
 };
 
 const resetPositions = (title) => {
-  title.parentNode.querySelector('section').style.height = 0;
   title.style.pointerEvents = "auto";
+  title.parentNode.classList.remove('no-height');
+  title.parentNode.querySelector('section').style.height = 0;
   title.parentNode.querySelector('section').style.opacity = 0;
-
-
   if (window.innerWidth <= 780) {
     document.querySelector('.menu-left').classList.remove('visible');
     document.querySelector('.languages').classList.remove('hidden');
   }
-    title.style.transform = "translateY(0)";
-    title.classList.remove('no-height');
-    title.parentNode.classList.remove('no-height');
+  title.style.transform = "translateY(0)";
 }
