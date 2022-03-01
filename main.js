5bfa075b8544800dc6fb70898b9c6119fa8ccb7a
Yoni Touboul <yonitouboul@hotmail.com>
2022-03-01 15:00:15 +0100

    Commented out future weezevent link (to update tomorrow)

--- main.js
+++ main.js
@@ -28,9 +28,9 @@ $.i18n()
 const setWeezeventSrc = () => {
   const locale = $.i18n().locale === "en" ? "en-GB" : "fr-FR";
   const iframe = document.querySelector(".weezevent2021");
-  iframe.src =
-    `https://widget.weezevent.com/ticket/E811570/?code=12629&locale=${locale}&width_auto=1&color_primary=00AEEF` +
+  iframe.src = `https://widget.weezevent.com/ticket/E789415/?code=47299&locale=${locale}&width_auto=1&color_primary=00AEEF` +
     iframe.dataset.key;
+    // `https://widget.weezevent.com/ticket/E811570/?code=12629&locale=${locale}&width_auto=1&color_primary=00AEEF`
 };
 
 
